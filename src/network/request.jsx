16491e2169aca91463fa7506e374301749aae5c8
veeck <michael.veeck@nebenan.de>
2022-10-13 19:56:29 +0200

    [CORE-10501] Cleanup logic

--- src/network/request.jsx
+++ src/network/request.jsx
@@ -97,10 +97,5 @@ export default (options) => {
     throw networkError;
   };
 
-  if (requestConfig.url?.includes('contentfull_proxy')) {
-    console.log('requesting from contentful via axios with requestConfig: ');
-    console.log(requestConfig);
-  }
-
   return axios(requestConfig).then(pipeResponse, rethrowError);
 };
--- src/promise/index.jsx
+++ src/promise/index.jsx
@@ -12,22 +12,34 @@ export const middleware = (store) => (
       if (!action.promise) return next(action);
       const { promise, type } = action;
       const state = store.getState();
+
       // prevent unnecessary calls
-      if (promise.shouldRequest && !promise.shouldRequest(state)) return Promise.resolve(null);
+      if (promise.shouldRequest && !promise.shouldRequest(state)) {
+        return Promise.resolve(null);
+      }
 
       const cleanAction = omit(action, 'promise');
       const newAction = { ...cleanAction };
       next(newAction);
 
-      promise.requestPromise
+      promise.promiseRequest
         .then((payload) => {
           const resolvedAction = { ...cleanAction, type: resolved(type), payload };
+
+          // prevent unnecessary state updates
+          if (promise.shouldFire && !promise.shouldFire(payload)) {
+            throw new Error(payload);
+          }
+
           next(resolvedAction);
         })
         .catch((payload) => {
           const rejectedAction = { ...cleanAction, type: rejected(type), payload };
           next(rejectedAction);
         });
+
+      // trigger the request
+      promise.promiseResolve();
 
       return promise;
     }
