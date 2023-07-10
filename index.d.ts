d6654a3e4c68a83c4c2b440b49bc2bc07d66bf28
veeck <michael.veeck@nebenan.de>
2023-07-10 08:04:52 +0200

    Remove useless declaration

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,3 @@ declare module 'local-storage-adapter' {
   export const get = (key: string) => Record<string, unknown>;
   export const set = (key: string, payload: Record<string, unknown>) => boolean;
 }
-
-declare module 'escape-string-regexp-browser' {
-  export default (domain: string) => string;
-}
